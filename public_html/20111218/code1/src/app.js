120fa8c1546ff0e2b0a69318f00257d3793fb7ac
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-12-16 08:16:41 +0900

    追加

---
+++ public_html/20111218/code1/src/app.js
@@ -0,0 +1,19 @@
+Ext.application({
+
+    // アプリケーション名
+    name: 'Xenophy',
+
+    // アプリケーションフォルダパス
+    appFolder: 'src/app',
+
+    // 使用コントローラー定義
+    controllers: [],
+
+    // アプリケーション起動時イベントハンドラ
+    launch: function() {
+
+        alert('Launch My Applicaton.');
+
+    }
+
+});
